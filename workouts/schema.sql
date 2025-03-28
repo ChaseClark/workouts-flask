57e8138546b2110a819511c3ebbca3d9c95fd8de
Chase Clark <chase_clark07@hotmail.com>
2025-03-27 22:15:03 -0400

    wip: add new tables

--- workouts/schema.sql
+++ workouts/schema.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS workout;
+DROP TABLE IF EXISTS exercise;
+DROP TABLE IF EXISTS workout_exercise;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -7,11 +10,39 @@ CREATE TABLE user (
   password TEXT NOT NULL
 );
 
+CREATE TABLE category (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
 CREATE TABLE workout (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
   notes TEXT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE exercise (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  category_id INTEGER,
+  name TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (category_id) REFERENCES category (id)
+);
+
+CREATE TABLE workout_exersise (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  workout_id INTEGER NOT NULL,
+  exercise_id INTEGER NOT NULL,
+  sets INTEGER,
+  reps INTEGER,
+  weight NUMERIC(4,2),
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (workout_id) REFERENCES workout (id),
+  FOREIGN KEY (exercise_id) REFERENCES exercise (id)
 );
