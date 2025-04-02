d8064620b927d6a050bffa3d78ca6a9f41e493ab
Chase Clark <chase_clark07@hotmail.com>
2025-04-01 21:55:22 -0400

    wip htmx

--- workouts/workouts.py
+++ workouts/workouts.py
@@ -39,7 +39,7 @@ def index():
 # TODO: to simplify things, workouts.create should just create a workout and
 # redirect the user to the edit page with the new id
 # this will reduce the amount of duplication required
-@bp.route("/create", methods=("GET", "POST"))
+@bp.route("/create", methods=("POST",))
 @login_required
 def create():
     if request.method == "POST":
@@ -58,7 +58,15 @@ def create():
             db.commit()
             # redirect to update so that user can add exercises to workout
             return redirect(url_for("workouts.update", id=last_id))
-    elif request.method == "GET":
+        return render_template("workouts/create_update.html", categories=g.categories)
+
+
+@bp.route("/<int:id>/detail", methods=("GET", "PUT"))
+@login_required
+def detail(id):
+    workout = get_workout(id)
+    session["workout_id"] = id
+    if request.method == "GET":
         db = get_db()
         exercises = db.execute(
             " SELECT id, name, category_id, user_id"
@@ -67,18 +75,15 @@ def create():
             " ORDER BY name",
             (g.user["id"],),
         ).fetchall()
-
-    return render_template(
-        "workouts/create_update.html", exercises=exercises, categories=g.categories
-    )
-
-
-@bp.route("/<int:id>/update", methods=("GET", "POST"))
-@login_required
-def update(id):
-    workout = get_workout(id)
-    session["workout_id"] = id
-    if request.method == "POST":
+        return render_template(
+            "workouts/detail.html",
+            workout=workout,
+            categories=g.categories,
+            exercises=exercises,
+            id=id,
+        )
+    elif request.method == "PUT":
+        print("update PUT request fired!")
         notes = request.form["notes"]
         error = None
 
@@ -92,21 +97,14 @@ def update(id):
             )
             db.commit()
             return redirect(url_for("workouts.index"))
-    elif request.method == "GET":
-        db = get_db()
-        exercises = db.execute(
-            " SELECT id, name, category_id, user_id"
-            " FROM exercise"
-            " WHERE user_id = ?"
-            " ORDER BY name",
-            (g.user["id"],),
-        ).fetchall()
-        return render_template(
-            "workouts/create_update.html",
-            workout=workout,
-            categories=g.categories,
-            exercises=exercises,
-        )
+
+
+# only editing the notes for now
+@bp.route("/<int:id>/edit", methods=("GET",))
+@login_required
+def edit(id):
+    workout = get_workout(id)
+    return render_template("workouts/edit_notes.html", workout=workout, id=id)
 
 
 @bp.route("/<int:id>/delete", methods=("POST",))
